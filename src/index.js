64ac12aa9eef85723f464440996480d357733c47
NguyenHoangTuanKiet <kietnguyen.op@gmai.com>
2019-04-08 17:17:11 +0700

    Show list item; each listitem can be removable

--- src/index.js
+++ src/index.js
@@ -2,9 +2,67 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+import { Provider } from 'react-redux'
+import store from './store'
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+class ListItem extends React.Component {
+  render() {
+    return(
+      <li>
+        { this.props.value.title }
+        <button onClick={()=>{this.props.onClick()}}>Remove</button>
+      </li>
+    );
+  }
+
+}
+
+
+class TodoList extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      listItems: [ {title: "Todo#1", completed: false}, {title: "Todo#2", completed: false}, {title: "Todo#3", completed: false}, {title: "Todo#4", completed: false}, {title: "Todo#5", completed: false} ]
+    }
+  }
+
+  handleRemoveClick(index) {
+    const listItems = this.state.listItems.slice();
+    listItems.splice(index, 1);
+    this.setState({listItems: listItems});
+  }
+
+  render() {
+
+    let listItems = this.state.listItems
+
+    return  (
+      <ul>
+        {
+          listItems.map((item, index) => {
+            return(
+              <ListItem value={item} onClick={ () => this.handleRemoveClick(index) } />
+            )
+          })
+        }
+      </ul>
+    );
+
+  }
+
+}
+
+
+ReactDOM.render(<Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
